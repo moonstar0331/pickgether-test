7c7ea8fbbf8d46050cf034fcb4af0dfe8522efd1
MoonStar <moonsung0331@gmail.com>
2023-02-05 16:42:15 +0900

    #5 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    
    지금은 프로젝트 초기화 단계이고, 사용 기술을 명확하게 정해지지 않았기 때문에 간단한 의존성만 추가하였음.
    
    * web
    * lombok
    
    추후 프로젝트를 진행하면서, 회의를 통해서 의존성을 추가할 예정이다.

---
+++ pickgether/src/main/java/com/tikitaka/pickgether/PickgetherApplication.java
@@ -0,0 +1,13 @@
+package com.tikitaka.pickgether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickgetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickgetherApplication.class, args);
+	}
+
+}
