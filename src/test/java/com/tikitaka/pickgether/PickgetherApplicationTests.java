857a475c63c26c32bfb9f94760e2abc9b21ff859
MoonStar <moonsung0331@gmail.com>
2023-02-12 23:06:50 +0900

    파일 경로 간편화

---
+++ src/test/java/com/tikitaka/pickgether/PickgetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tikitaka.pickgether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickgetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
